60d21cd996a6934db72bfc60de0a6a4040b371d3
Joyce-D1992 <lemosjoyce01@gmail.com>
2023-12-16 16:11:18 -0500

    Studied use of class based components

--- simpleHelloWorld/src/components/About.ts.tsx
+++ simpleHelloWorld/src/components/About.ts.tsx
@@ -1,7 +1,12 @@
+import {User} from "./User";
+import {UserClass} from "./UserClass";
+
 export const About = () => {
     return (
         <div className="aboutus">
             About us
+            <User name="Manish Dsouza (functional)" location="Mumbai" contact="dsouzamanish@gmail.com"/>
+            <UserClass name="Manish Dsouza (class)" location="Mumbai" contact="dsouzamanish@gmail.com"/>
         </div>
     )
 }
