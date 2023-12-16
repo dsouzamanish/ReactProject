2e58a087e00f43597481e055ce34656733d6e857
dsouzamanish <dsouzamanish@gmail.com>
2023-12-16 18:55:51 -0500

    Studied use of class based components

--- simpleHelloWorld/src/components/RestaurantMenu.ts.tsx
+++ simpleHelloWorld/src/components/RestaurantMenu.ts.tsx
@@ -8,7 +8,6 @@ export const RestaurantMenu = () => {
         const response= await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.113645&lng=72.8697339&restaurantId="+resId);
         const data = await response.json();
         const itemCards =data?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards;
-        console.log(itemCards)
         const {name, costForTwoMessage, avgRatings, cuisines} = data?.data?.cards[0].card?.card?.info;
         const resDataFromApi = {name, costForTwoMessage, avgRatings, cuisines, itemCards};
         setResData(resDataFromApi);
