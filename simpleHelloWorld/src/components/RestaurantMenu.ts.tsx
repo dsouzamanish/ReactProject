d674852e1e67ea07aa5b52bf7360dce119f2107a
dsouzamanish <dsouzamanish@gmail.com>
2023-12-16 19:14:14 -0500

    added custom hooks implementation

--- simpleHelloWorld/src/components/RestaurantMenu.ts.tsx
+++ simpleHelloWorld/src/components/RestaurantMenu.ts.tsx
@@ -1,21 +1,9 @@
-import {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom'
 import {ShimmerComponent} from "./Shimmer";
+import {useRestraurantMenu} from "../hooks/useRestraurantMenu";
 export const RestaurantMenu = () => {
     const {resId}  = useParams();
-    const [resData, setResData] = useState({});
-    const fetchMenu = async () => {
-        const response= await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.113645&lng=72.8697339&restaurantId="+resId);
-        const data = await response.json();
-        const itemCards =data?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards;
-        const {name, costForTwoMessage, avgRatings, cuisines} = data?.data?.cards[0].card?.card?.info;
-        const resDataFromApi = {name, costForTwoMessage, avgRatings, cuisines, itemCards};
-        setResData(resDataFromApi);
-    }
-    useEffect(() => {
-      fetchMenu();
-    }, []);
-
+    const resData = useRestraurantMenu(resId);
     if(resData === {}) {
         return <ShimmerComponent />
     }
---
+++ simpleHelloWorld/src/hooks/useRestraurantMenu.tsx
@@ -0,0 +1,16 @@
+import {useEffect, useState} from 'react'
+export const useRestraurantMenu = (resId: string) => {
+    const [resData, setResData] = useState({})
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const response = fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=19.113645&lng=72.8697339&restaurantId="+resId);
+        const data = await response.json();
+        const itemCards =data?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards;
+        const {name, costForTwoMessage, avgRatings, cuisines} = data?.data?.cards[0].card?.card?.info;
+        const resDataFromApi = {name, costForTwoMessage, avgRatings, cuisines, itemCards};
+        setResData(resDataFromApi);
+    }
+}
