d86d317bd79d3f720185d61fe3d5f3ece9c7d2a7
dsouzamanish <dsouzamanish@gmail.com>
2023-12-16 20:45:52 -0500

    Added lazy loading

--- simpleHelloWorld/src/app.js
+++ simpleHelloWorld/src/app.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, {lazy, Suspense} from "react";
 import ReactDOM from "react-dom/client";
 import {HeaderComponent} from "./components/Header.ts.tsx";
 import {RestaurantContainer} from "./components/RestaurantCard";
 import {createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
 import {About} from "./components/About.ts";
-import {ContactUs} from "./components/ContactUs.ts";
 import {Error} from "./components/Error.ts";
 import {RestaurantMenu} from "./components/RestaurantMenu.ts";
 
@@ -18,7 +17,7 @@ const AppComponent = () => {
     )
 }
 
-
+const ContactUs = lazy(() => import("./components/ContactUs.ts"));
 const router = createBrowserRouter([{
     path: '/',
     element: <AppComponent />,
@@ -30,7 +29,7 @@ const router = createBrowserRouter([{
         element: <About />
     },{
         path: '/contact',
-        element: <ContactUs />
+        element: <Suspense fallback={<h2>Loading ....</h2>}><ContactUs /></Suspense>
     },{
         path: '/restaurant/:resId',
         element: <RestaurantMenu />
--- simpleHelloWorld/src/components/ContactUs.ts.tsx
+++ simpleHelloWorld/src/components/ContactUs.ts.tsx
@@ -1,7 +1,8 @@
-export const ContactUs = () => {
+ const ContactUs = () => {
     return (
         <div className='contactUs'>
             Contact us at: Dsouzamanish@gmail.com
         </div>
     )
 }
+ export default ContactUs
